44a3c88e68ddd41586f540b561944c68d3820921
Mustapha Bookob <mustapha.bookob@student.rocva.nl>
2021-11-24 10:51:15 +0100

    2.6

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -0,0 +1,18 @@
+<?php
+    $voornaam = "Tom ";
+    echo gettype($voornaam);
+
+    $leeftijd = 17 ;
+    echo gettype($leeftijd);
+
+    $c = 3.2 ;
+    echo gettype($c);
+
+    $d = false ;
+    echo gettype($d);
+
+    $e = NULL ;
+    echo gettype($e);
+
+
+?>
