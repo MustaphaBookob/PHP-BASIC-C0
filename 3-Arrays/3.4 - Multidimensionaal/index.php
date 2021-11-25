0d15ff4a77b372a5b98b1a94a2560345c33345e6
Mustapha Bookob <mustapha.bookob@student.rocva.nl>
2021-11-25 23:04:07 +0100

    3.4 en 3,5

---
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -0,0 +1,9 @@
+<?php
+    $abcGetallen = [ [24,56,78,90], ["a", "b", "c", "d",], true, false ];  
+    
+    print_r($abcGetallen);
+
+    echo "<br>" . $abcGetallen[1][0]. $abcGetallen[0][1]. "<br>";
+    echo $abcGetallen[1][2]. $abcGetallen[0][3];
+
+?>
---
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$getallen= [676,5345,78657,3,46,21,47,];
+$laag_hoog = asort($getallen);
+print_r($getallen);
+
+$getallen= [676,5345,78657,3,46,21,47,];
+$hoog_laag = arsort($getallen);
+print_r($getallen);
+
+?>
