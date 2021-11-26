f8181921db89f4fa714059b862d9b2b49766badf
Mustapha Bookob <mustapha.bookob@student.rocva.nl>
2021-11-26 09:32:33 +0100

    4.1

---
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$getal1 = 5;
+$getal2= 20;
+
+echo $getal1 + $getal2 . "<br>"; 
+echo $getal1 - $getal2 . "<br>"; 
+echo $getal1 * $getal2 . "<br>"; 
+echo $getal1 / $getal2 . "<br>"; 
+
+?>
