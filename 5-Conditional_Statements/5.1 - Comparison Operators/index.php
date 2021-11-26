be87efa7251eac1cab924b73b9a263ad5cd539c3
Mustapha Bookob <mustapha.bookob@student.rocva.nl>
2021-11-26 22:22:23 +0100

    5.1

---
+++ 5-Conditional_Statements/5.1 - Comparison Operators/index.php
@@ -0,0 +1,22 @@
+<?php
+$test1 = 12;
+    $test2 = "gewoontekst";
+
+    var_dump($test1 == $test2);
+    echo "<br>";
+    var_dump($test1 === $test2); 
+    echo "<br>";
+    var_dump($test1 != $test2);
+    echo "<br>";
+    var_dump($test1 > $test2);
+    echo "<br>";
+    var_dump($test1 < $test2);
+    echo "<br>";
+    var_dump($test1 <> $test2);
+    echo "<br>";
+    var_dump($test1 >= $test2);
+    echo "<br>";
+    var_dump($test1 <= $test2);
+    echo "<br>";
+    var_dump($test1 > $test2);
+?>
