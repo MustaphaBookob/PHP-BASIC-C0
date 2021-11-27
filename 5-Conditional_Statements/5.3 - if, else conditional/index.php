809da877698eae22b99a04d6a88ff3bc137f22d1
Mustapha Bookob <mustapha.bookob@student.rocva.nl>
2021-11-27 15:32:25 +0100

    5.3

---
+++ 5-Conditional_Statements/5.3 - if, else conditional/index.php
@@ -0,0 +1,66 @@
+<?php
+
+$test1 = 3;
+$test2 = 6;
+
+if($test1 == $test2) {
+    echo "gelijk";
+}
+else {
+    echo "ongelijk";
+}
+
+if($test1 != $test2) {
+    echo "ongelijk";
+}
+else {
+    echo "gelijk";
+}
+
+echo "<br>";
+
+
+if($test1 === $test2) {
+    echo "identiek";
+}
+else {
+    echo "niet identiek";
+}
+ 
+echo  "<br>";
+
+if($test1 > $test2) {
+    echo "test1 is groter dan test2";
+}
+else {
+    echo "test1 is kleiner dan test2";
+}
+
+echo "<br>";
+
+if($test1 < $test2) {
+    echo "test1 is kleiner dan test2";
+}
+else {
+    echo "test1 is groter dan test2";
+}
+
+echo "<br>";
+
+if($test1 >= $test2) {
+    echo "test1 is groter of gelijk aan test2";
+}
+else { 
+    echo "test1 is kleiner of gelijk aan test2";
+}
+
+echo "<br>";
+
+if($test1 <= $test2) {
+    echo "test1 is kleiner of gelijk aan test2";
+}
+else { 
+    echo "test1 is groter of gelijk aan test2";
+}
+
+?>
