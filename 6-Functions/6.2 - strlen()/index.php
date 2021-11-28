6c1e275648f8eaf1a9c9c7af03af8a231dd85b18
Mustapha Bookob <mustapha.bookob@student.rocva.nl>
2021-11-28 18:46:15 +0100

    6.2

---
+++ 6-Functions/6.2 - strlen()/index.php
@@ -0,0 +1,11 @@
+<?php
+
+function MijnNaamIsGroot(){
+    $voornaam = "Mustapha";
+    $achternaam = "Bookob";
+    $gehelenaam = "mijn naam is $voornaam  $achternaam";
+    echo strtoupper ($gehelenaam); 
+}
+
+MijnNaamIsGroot(); 
+
